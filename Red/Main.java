36b240e7a48ef097b3689b0870559706d6231d8a
JoseUnahSys <mi.visual.studio.acount@outlook.es>
2025-04-08 16:48:51 -0600

    Update Main.java

--- Red/Main.java
+++ Red/Main.java
@@ -8,19 +8,25 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.awt.image.Raster;
-
-
-
 import java.util.Random;
+
+
+class Image {
+    int width, height;
+    double[] data;
+
+    Image(int width, int height, double[] data) {
+        this.width = width;
+        this.height = height;
+        this.data = data;
+    }
+}
 
 public class Main {
 
-    public static void main(String[] args) throws Exception
+    public static Image CargarImagen(String nombre) throws Exception
     {
-        System.out.println("Cargando Imagen y convirtiendo a valores flotantes (double).\n");
-
-        File file = new File("./persona_gris_160x120.jpg");
-        // File file = new File("./cachorro_gris_160x120.jpg");
+        File file = new File(nombre);
 
         BufferedImage img = ImageIO.read(file);
         int width = img.getWidth();
@@ -44,51 +50,74 @@ public class Main {
             for (int j = 0; j < height; j++)
             {
                 img_data[(i * height) + j] = ((double)(imgArr[i][j])) / 255.0;
-                // System.out.printf("%s ", img_data[(i * width) + j]);
+                // img_data[(i * height) + j] = ((double)(imgArr[i][j])) ;
+                // System.out.printf("%s ", img_data[(i * height) + j]);
             }
             // System.out.println("\n ");
         }
 
+        return new Image(width, height, img_data);
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        System.out.println("Cargando Imagenes y convirtiendo a valores flotantes (double).\n");
+
+        Image imagen1 = CargarImagen("./persona_gris_160x120.jpg");
+        Image imagen2 = CargarImagen("./cachorro_gris_160x120.jpg");
+        Image imagen3 = CargarImagen("./ave_gris_160x120.jpeg");
+
         // Para el Entrenamiento
         ArrayList<double[]> entradas = new ArrayList<double[]>();
         ArrayList<double[]> salidas = new ArrayList<double[]>();
+        entradas.add(imagen1.data); entradas.add(imagen1.data);
+        entradas.add(imagen1.data); entradas.add(imagen1.data);
+        entradas.add(imagen1.data); entradas.add(imagen1.data);
+        entradas.add(imagen1.data); entradas.add(imagen1.data);
+        entradas.add(imagen1.data); entradas.add(imagen1.data);
+        entradas.add(imagen1.data); entradas.add(imagen1.data);
+        salidas.add(new double[] { 1.0 }); salidas.add(new double[] { 1.0 });
+        salidas.add(new double[] { 1.0 }); salidas.add(new double[] { 1.0 });
+        salidas.add(new double[] { 1.0 }); salidas.add(new double[] { 1.0 });
+        salidas.add(new double[] { 1.0 }); salidas.add(new double[] { 1.0 });
+        salidas.add(new double[] { 1.0 }); salidas.add(new double[] { 1.0 });
+        salidas.add(new double[] { 1.0 }); salidas.add(new double[] { 1.0 });
 
-        entradas.add(img_data);
-        salidas.add(img_data);
+        RedNeuronal nn = new RedNeuronal(new int[]
+        {
+            imagen1.width * imagen1.height,
+            50,
+            10,
+            1
+        });
 
-        RedNeuronal nn = new RedNeuronal(new int[]{ width * height, 20, 1 });
-        double[] prediccionInicial = nn.Activacion(img_data); // Primer prediccion
+        double[] prediccionInicial = nn.Activacion(imagen1.data); // Primer prediccion
+        System.out.printf("-- Resultado inicial con imagen1 de la Red Neuronal %.16f\n", prediccionInicial[0]);
+        prediccionInicial = nn.Activacion(imagen2.data); // Primer prediccion
+        System.out.printf("-- Resultado inicial con imagen2 de la Red Neuronal %.16f\n", prediccionInicial[0]);
 
-        System.out.printf("--Resultado inicial de la Red Neuronal %.8f\n", prediccionInicial[0]);
-
-        // Para hacer la prueba final
-        ArrayList<double[]> X = new ArrayList<double[]>();
-        ArrayList<double[]> Y = new ArrayList<double[]>();
-        X.add(img_data);
-        Y.add(img_data);
         System.out.println("----- Comenzando Entrenamiento... ----- \n");
 
-        // nn.EntrenarYEncontrarEpoch(entradas, salidas, 0.5, 0.01);
+        nn.EntrenarYEncontrarEpoch(entradas, salidas, 0.5, 0.05);
 
-        int epoch = 10;
-        for (int e = 0; e < epoch; e++)
-        {
-            double error = nn.Entrenar(entradas, salidas, 0.5);
-            System.out.printf("ephoc = %s, error calculado = %.16f, error inverso = %.16f\n", e, error, 1.0 - error);
-        }
+        // int epoch = 10;
+        // for (int e = 0; e < epoch; e++)
+        // {
+        //     double error = nn.Entrenar(entradas, salidas, 0.5); // Ritmo de aprendizaje
+        //     System.out.printf("ephoc = %s, error calculado = %.16f\n", e, error);
+        // }
+
         System.out.println("----- Entrenamiento terminado ----- \n");
 
         System.out.println("----- Haciendo una prueba con la imagen ----- \n");
-        double err = nn.ErrorTotal(X, Y);
-        double[] res = nn.Activacion(img_data); // Haciendo una ultima prediccion
-        System.out.printf("resultado de la Red Neuronal, Activacion %.16f, Error %.16f\n", res[0], err);
+        // Para hacer la prueba final
+        double[] res = nn.Activacion(imagen1.data); // Haciendo una ultima prediccion
+        double err = nn.Error(res, new double[] { 1.0 });
+        System.out.printf("Activacion de la Red Neuronal con imagen1 (correcta) %.16f, Error %.16f\n", res[0], err);
 
-        // segun https://en.wikipedia.org/wiki/Sigmoid_function
-        if(res[0] > 0.0) {
-            System.out.printf("La Imagen SI es correcta.\n");
-        } else {
-            System.out.printf("La Imagen NO es correcta.\n");
-        }
+        double[] res2 = nn.Activacion(imagen2.data); // Haciendo una prediccion con imagen incorrecta
+        err = nn.Error(res2, new double[] { 0.0 });
+        System.out.printf("Activacion de la Red Neuronal con imagen2 (incorrecta), %.16f, Error %.16f\n", res2[0], err);
     }
 
 }
