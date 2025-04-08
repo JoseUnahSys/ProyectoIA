30f8e5b8412e64b50c523bd6d637ed6db7dbf77e
JoseUnahSys <mi.visual.studio.acount@outlook.es>
2025-04-08 16:48:21 -0600

    Update Neurona.java

--- Red/Neurona.java
+++ Red/Neurona.java
@@ -11,13 +11,13 @@ public class Neurona {
 
     Neurona(int numEntradas, Random r)
     {
-        sesgo = r.nextDouble();
-        // sesgo = 0.1;
+        // sesgo = r.nextDouble();
+        sesgo = -0.5;
         pesos = new double[numEntradas];
 
         for(int i = 0; i < pesos.length; i++) {
-            pesos[i] = r.nextDouble();
-            // pesos[i] = 0.0;
+            // pesos[i] = r.nextDouble();
+            pesos[i] = -0.5;
         }
     }
 
@@ -25,14 +25,14 @@ public class Neurona {
     {
         sumaActual = sesgo;
 
-        for(int i = 0; i < entradas.length; i++){
+        for(int i = 0; i < entradas.length; i++) {
             sumaActual += entradas[i] * pesos[i];
         }
 
         return Sigmoid(sumaActual); // Funcion de activacion, usando Sigmoid
     }
 
-    public double Sigmoid(double x){
+    public double Sigmoid(double x) {
         return 1 / (1 + Math.exp(-x));
     }
 }
