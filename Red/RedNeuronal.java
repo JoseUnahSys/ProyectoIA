a6ab5354c77cdbe38d5c61f9239d68a9c801af15
JoseUnahSys <mi.visual.studio.acount@outlook.es>
2025-04-10 07:51:56 -0600

    [RedNeuronal]: Reparando posible problema de memoria

--- Red/RedNeuronal.java
+++ Red/RedNeuronal.java
@@ -12,34 +12,60 @@ public class RedNeuronal {
 
     public RedNeuronal(int[] numNeuronasPorCapa)
     {
-        capas = new ArrayList<Capa>();
+        capas = new ArrayList<Capa>(numNeuronasPorCapa.length);
         Random r = new Random();
 
         for(int i = 0; i < numNeuronasPorCapa.length; i++)
         {
-            if(i == 0){
+            if(i == 0) {
                 capas.add(new Capa(numNeuronasPorCapa[i], numNeuronasPorCapa[i], r));
-            }else{
+            } else {
                 capas.add(new Capa(numNeuronasPorCapa[i - 1], numNeuronasPorCapa[i], r));
             }
         }
+
+        sigmas = new ArrayList<double[]>(capas.size());
+        for(int i = 0; i <  capas.size(); i++){
+            sigmas.add(new double[capas.get(i).neuronas.size()]);
+        }
+
+        deltas = new ArrayList<double[][]>(capas.size());
+        for(int i = 0; i < capas.size(); i++)
+        {
+            deltas.add(new double[capas.get(i).neuronas.size()][capas.get(i).neuronas.get(0).pesos.length]);
+        }
+
     }
 
 
-    public double Sigmoid(double x){
+    public double Sigmoid(double x) {
         return 1 / (1 + Math.exp(-x));
     }
 
-    public double SigmoidDerivada(double x){
+    public double SigmoidDerivada(double x) {
         double y = Sigmoid(x);
         return y*(1 - y);
     }
 
-    public double[] Activacion(double[] entradas){
+    public double ReLu(double x) {
+        return Math.max(x, 0);
+    }
+
+    public double ReLuDerivada(double x) {
+        if (x >= 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public double[] Activacion(double[] entradas) {
         double[] salidas = new double[0]; // Creando solo un apuntador
-        for(int i = 0; i < capas.size(); i++){
+        for(int i = 0; i < capas.size(); i++) {
+            // System.out.printf("[RedNeuronal]: capa[%s] {\n", i);
             salidas = capas.get(i).Activacion(entradas);
             entradas = salidas;
+            // System.out.printf("}\n", i);
         }
         return salidas;
     }
@@ -48,29 +74,25 @@ public class RedNeuronal {
     public double Error(double[] salidaReal, double[] salidaEsperada)
     {
         double error_acumulado = 0;
-        for(int i = 0; i < salidaReal.length; i++){
+        for(int i = 0; i < salidaReal.length; i++) {
             error_acumulado += 0.5 * Math.pow(salidaReal[i] - salidaEsperada[i], 2);
         }
-
         return error_acumulado;
     }
 
-    public double ErrorTotal(ArrayList<double[]> entradas, ArrayList<double[]> salidaEsperada){
+    public double ErrorTotal(ArrayList<double[]> entradas, ArrayList<double[]> salidaEsperada)
+    {
         double error_acumulado = 0;
         for(int i = 0; i < entradas.size(); i++) {
             error_acumulado += Error(Activacion(entradas.get(i)), salidaEsperada.get(i));
         }
-
         return error_acumulado;
     }
 
     public void iniciarDeltas()
     {
-        deltas = new ArrayList<double[][]>();
-
         for(int i = 0; i < capas.size(); i++)
         {
-            deltas.add(new double[capas.get(i).neuronas.size()][capas.get(i).neuronas.get(0).pesos.length]);
             for(int j = 0; j < capas.get(i).neuronas.size(); j++)
             {
                 for(int k = 0; k < capas.get(i).neuronas.get(0).pesos.length; k++)
@@ -88,22 +110,19 @@ public class RedNeuronal {
     */
     public void calcularSigmas(double[] salidaEsperada)
     {
-        sigmas = new ArrayList<double[]>();
-        for(int i = 0; i <  capas.size(); i++){
-            sigmas.add(new double[capas.get(i).neuronas.size()]);
-        }
-
         for(int i = capas.size() - 1; i >= 0; i--) {
             for(int j = 0; j < capas.get(i).neuronas.size(); j++){
-                if(i == capas.size() - 1){
+                if(i == capas.size() - 1) {
                     double y = capas.get(i).salidas[j];
                     sigmas.get(i)[j] = (y - salidaEsperada[j]) * SigmoidDerivada(y);
-                }else{
+                    // sigmas.get(i)[j] = (y - salidaEsperada[j]) * ReLuDerivada(y);
+                } else {
                     double sum = 0;
                     for(int k = 0; k < capas.get(i + 1).neuronas.size(); k++){
                         sum += capas.get(i + 1).neuronas.get(k).pesos[j] * sigmas.get(i + 1)[k];
                     }
                     sigmas.get(i)[j] = SigmoidDerivada(capas.get(i).neuronas.get(j).sumaActual) * sum;
+                    // sigmas.get(i)[j] = ReLuDerivada(capas.get(i).neuronas.get(j).sumaActual) * sum;
                 }
             }
         }
@@ -153,7 +172,8 @@ public class RedNeuronal {
         iniciarDeltas();
         for(int i = 0; i < entradas.size(); i++)
         {
-            Activacion(entradas.get(i));
+            double[] activacion = Activacion(entradas.get(i));
+            System.out.printf("[RedNeuronal]: activacion = %.16f\n", activacion[0]);
             calcularSigmas(salidaEsperada.get(i));
             calcularDeltas();
             activarSesgos(alfa);
@@ -167,11 +187,11 @@ public class RedNeuronal {
 
         double error = 99999999;
         int epoch = 0;
-        while(error > maxError)
+        while (error > maxError)
         {
-            System.out.printf("ephoc = %s, error = %.8f\n", epoch, error);
-            RetroPropagar(entradasPruebas, salidasPruebas, alfa);
+            RetroPropagar(entradasPruebas, salidasPruebas, alfa );
             error = ErrorTotal(entradasPruebas, salidasPruebas);
+            System.out.printf("ephoc = %s, error = %.16f\n", epoch, error);
             epoch++ ;
         }
     }
@@ -186,4 +206,3 @@ public class RedNeuronal {
 
 
 }
-
